62656a307c3656ba50ce85e5af904a1dd22b6a65
ElCanari <35748017+ElCanari@users.noreply.github.com>
2018-01-24 06:42:28 -0500

    Delete rlist.js

--- commands/rlist.js
+++
@@ -1,6 +0,0 @@
-exports.run = (client, message, args) => {
-let args4 = args[0];
-let roleID = message.guild.roles.find("name", args4);
-let membersWithRole = message.guild.roles.get(roleID).members;
-message.channel.send(`Il y a ${membersWithRole.size} membres avec ce role`);
-}
