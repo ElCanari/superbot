7ed63909c44511a09cc2cf73598d254bc5636671
ElCanari <35748017+ElCanari@users.noreply.github.com>
2018-01-23 20:36:55 -0500

    Add files via upload

---
+++ commands/piaf.js
@@ -0,0 +1,7 @@
+   exports.run = (client, message, args) => {
+
+  message.channel.send(":baby_chick: > all", {
+    file: "https://t2.ea.ltmcdn.com/fr/images/5/9/5/img_les_soins_du_canari_595_600.jpg" // Or replace with FileOptions object
+
+  });
+  }
