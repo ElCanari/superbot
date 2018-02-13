03f7323192fb4340837ec15392d3958e5798abd9
ElCanari <35748017+ElCanari@users.noreply.github.com>
2018-02-13 09:27:42 -0500

    Update hr.js

--- commands/hr.js
+++ commands/hr.js
@@ -1,40 +1,31 @@
    exports.run = (client, message, args) => {
 const fs = require("fs");
+const talkedRecently = new Set();
+var temp = 60;
 
-let hr = JSON.parse(fs.readFileSync("./hr.json", "utf8"));
 const config = require("./config.json");
+
+// Inside your message event, this code will stop any command during cooldown.
+// Should be placed after your code that checks for bots & prefix, for best performance
+
+if(var temp == "60")
+{
+    setTimeout(Timer, 1000)
+    message.channel.send("60 seconde timer test")    
+  
+      }
+
+
+function Timer() 
+{
+ temp--;
+ setTimeout(timer,1000)   
+    }
+    
+    
+message.channel.send("Vous devez attendre " + temp)
 
 if (!message.content.startsWith(config.prefix)) return;
   if (message.author.bot) return;
 
- if (!hr[message.author.id]) hr[message.author.id] = {
-    points: 0,
-    temp: 0,
-	seconde: 0,
-  };
-
-
-    let userData2 = hr[message.author.id];
-  if(userData2.temp == "0")
-  {
-	  userData2.temp += 60;
-	  userData2.seconde += 61;
-	  setTimeout(timer, 1000)
-	  function timer {
-		  if(userData2.seconde == "1")
-		  {
-			  userData2.temp --;
-			  userData2.seconde += 61;
-		  }
-		  userData2.seconde --;
-		  setTimeout(timer, 1000)
-	  }
-	  
-  }
-  if(userData2.temp !== "0")
-  {message.channel.send(`Revenez dans ${userData2.temp} et ${userData2.seconde} secondes. `)}
-
-  fs.writeFile("./hr.json", JSON.stringify(hr), (err) => {
-    if (err) console.error(err)
-  });
   }
