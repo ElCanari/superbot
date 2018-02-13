7a9bdc32a9ee6e05d0f1d1c2da96ce100b31fb4a
ElCanari <35748017+ElCanari@users.noreply.github.com>
2018-02-13 09:28:34 -0500

    Update hr.js

--- commands/hr.js
+++ commands/hr.js
@@ -8,7 +8,7 @@ const config = require("./config.json");
 // Inside your message event, this code will stop any command during cooldown.
 // Should be placed after your code that checks for bots & prefix, for best performance
 
-if(var temp == "60")
+if(temp == "60")
 {
     setTimeout(Timer, 1000)
     message.channel.send("60 seconde timer test")    
